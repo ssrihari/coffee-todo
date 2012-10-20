f23667601aade362f1776949798273b68a0fb5ff
Srihari Sriraman <srihari@c42.in>
2012-10-20 23:50:04 +0530

    Fetched all items on load.
    Added Item.init that fetches all items from db.
    Added case in constructor to accept 2 arguments.
    Created Items on load.
    Incremented the Item counter aptly.

--- item.php
+++ item.php
@@ -1,23 +1,50 @@
 <?php
 
-  if( isset($_POST['data']) ) {
-    $item_data = $_POST['data'];
-    addItem($item_data);
-    echo "saved $item_data";
-  }
+if( isset($_POST['data']) ) {
+  $item_data = $_POST['data'];
+  addItem($item_data);
+  echo "saved $item_data";
+}
 
-  function addItem($data='')
-  {
-    $host="localhost";
-    $username="root";
-    $password="srihari";
-    $db_name="todo";
-    $table_name="items";
+if( isset($_GET['all']) ) {
+  allItems();
+}
 
-    $con = mysql_connect("$host", "$username", "$password")or die("cannot connect");
-    mysql_select_db("$db_name")or die("cannot select DB");
-    $query = "INSERT INTO $table_name (data) VALUES ('$data')";
-    mysql_query($query);
-    mysql_close($con);
+function connect() {
+  $host="localhost";
+  $username="root";
+  $password="srihari";
+  $db_name="todo";
+
+  $con = mysql_connect("$host", "$username", "$password")or die("cannot connect");
+  mysql_select_db("$db_name")or die("cannot select DB");
+  return $con;
+}
+
+function mysql_fetch_all($result) {
+  $rows = array();
+  while($r = mysql_fetch_assoc($result)) {
+    $rows[] = $r;
   }
+  print json_encode($rows);
+}
+
+function addItem($data='') {
+  $table_name="items";
+  $con = connect();
+  $query = "INSERT INTO $table_name (data) VALUES ('$data')";
+  mysql_query($query);
+  mysql_close($con);
+}
+
+function allItems() {
+  $table_name="items";
+  $con = connect();
+  $query = "SELECT * FROM $table_name";
+  $result = mysql_query($query);
+  $res = mysql_fetch_all($result);
+  mysql_free_result($result);
+  mysql_close($con);
+  return $res;
+}
 ?>
--- src/item.coffee
+++ src/item.coffee
@@ -1,9 +1,28 @@
 class @Item
   @COUNT: 0
-  constructor: () ->
+
+  constructor: (id, data) ->
+    Item.COUNT++
     @input = $('<input class="item" type="text"/>')
-    @input.attr('id', "item-#{Item.COUNT++}")
-    $(".list").append(@input)
+    switch arguments.length
+      when 0
+        @input.attr('id', "item-#{Item.COUNT}")
+        $(".list").append(@input)
+      when 2
+        @input.attr('id', "item-#{id}")
+        @input.val(data)
+        $(".list").append(@input)
+
 
   content: ->
     $(this.input).val()
+
+  @init: ->
+    $.get "item.php",
+      all: "true"
+    , (data) ->
+      data = JSON.parse data
+      for i, item of data
+        console.log(item['id']+'-'+item['data'])
+        new Item(item['id'], item['data'])
+
