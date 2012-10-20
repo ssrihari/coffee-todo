e3e26388b213a2808acbf01068831c656ec43b11
Srihari Sriraman <srihari@c42.in>
2012-10-20 21:02:32 +0530

    Refactored to addItem in a function.

---
+++ item.php
@@ -0,0 +1,20 @@
+<?php
+  $item_data = $_POST['data'];
+  addItem($item_data);
+  echo "saved $item_data";
+
+  function addItem($data='')
+  {
+    $host="localhost";
+    $username="root";
+    $password="srihari";
+    $db_name="todo";
+    $table_name="items";
+
+    $con = mysql_connect("$host", "$username", "$password")or die("cannot connect");
+    mysql_select_db("$db_name")or die("cannot select DB");
+    $query = "INSERT INTO $table_name (data) VALUES ('$data')";
+    mysql_query($query);
+    mysql_close($con);
+  }
+?>
