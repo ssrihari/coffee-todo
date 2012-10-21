5e8f43d12530f32f797743ea7c11f02008b41852
Srihari Sriraman <srihari@c42.in>
2012-10-21 21:50:30 +0530

    Modified db to specify foreign key relationship and dependent destroy

--- initdb.sql
+++ initdb.sql
@@ -1,13 +1,14 @@
 create schema todo;
 use todo;
 
-create table items(
-  id INT NOT NULL AUTO_INCREMENT primary key,
-  data varchar(200),
-  list_id INT NOT NULL
-);
-
 create table lists(
   id INT NOT NULL AUTO_INCREMENT primary key,
   name varchar(200)
-);
+) ENGINE = INNODB;
+
+create table items(
+  id INT NOT NULL AUTO_INCREMENT primary key,
+  data varchar(200),
+  list_id INT NOT NULL,
+  FOREIGN KEY (list_id) REFERENCES lists(id) ON DELETE CASCADE
+) ENGINE = INNODB;
